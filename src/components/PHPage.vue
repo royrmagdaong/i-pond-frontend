a3e9ed52467f9f4cc2ac1bbd6a6a079f8cf697d6
Roy <royrmagdaong@gmail.com>
2023-08-04 15:58:57 +0800

    add date filter, pagination, sort by asc and desc in Sensor Data History Table

--- src/components/PHPage.vue
+++ src/components/PHPage.vue
@@ -3,6 +3,14 @@
     <BackButton class="q-mt-lg q-ml-md" />
     <div class="row q-col-gutter-sm q-pa-sm">
       <div class="col-12">
+        <q-card
+          bordered
+          flat
+          style="height: 50px; width: 100%"
+          class=""
+        ></q-card>
+      </div>
+      <div class="col-12">
         <q-card bordered flat style="height: 100%; width: 100%" class="">
           <q-card-section
             class="q-pa-lg text-overline"
@@ -25,6 +33,95 @@
         </q-card>
       </div>
       <div class="col-12">
+        <q-card
+          bordered
+          flat
+          style="width: 100%"
+          class="flex justify-end q-pa-sm"
+        >
+          <div class="q-mr-sm sort-opt">
+            <q-select
+              dense
+              outlined
+              v-model="sort"
+              :options="sort_options"
+              @update:model-value="changeQueryParams"
+            />
+          </div>
+          <div class="q-mr-sm date-from">
+            <q-input
+              dense
+              outlined
+              v-model="date_from"
+              label="Date from"
+              mask="date"
+              :rules="['date']"
+              hide-bottom-space
+            >
+              <template v-slot:append>
+                <q-icon name="event" class="cursor-pointer">
+                  <q-popup-proxy
+                    cover
+                    transition-show="scale"
+                    transition-hide="scale"
+                  >
+                    <q-date
+                      v-model="date_from"
+                      @update:model-value="changeQueryParams"
+                    >
+                      <div class="row items-center justify-end">
+                        <q-btn
+                          v-close-popup
+                          label="Close"
+                          color="primary"
+                          flat
+                        />
+                      </div>
+                    </q-date>
+                  </q-popup-proxy>
+                </q-icon>
+              </template>
+            </q-input>
+          </div>
+
+          <div class="date-to">
+            <q-input
+              dense
+              outlined
+              v-model="date_to"
+              label="Date to"
+              mask="date"
+              :rules="['date']"
+              hide-bottom-space
+            >
+              <template v-slot:append>
+                <q-icon name="event" class="cursor-pointer">
+                  <q-popup-proxy
+                    cover
+                    transition-show="scale"
+                    transition-hide="scale"
+                  >
+                    <q-date
+                      v-model="date_to"
+                      @update:model-value="changeQueryParams"
+                    >
+                      <div class="row items-center justify-end">
+                        <q-btn
+                          v-close-popup
+                          label="Close"
+                          color="primary"
+                          flat
+                        />
+                      </div>
+                    </q-date>
+                  </q-popup-proxy>
+                </q-icon>
+              </template>
+            </q-input>
+          </div>
+        </q-card>
+      </div>
+      <div class="col-12">
         <q-table
           :rows="rows"
           :columns="columns"
@@ -32,7 +129,9 @@
           flat
           bordered
           :pagination="initialPagination"
+          :loading="loading"
         >
+          <!--  -->
           <template v-slot:body-cell-ph="props">
             <q-td :props="props" class="">
               {{ props.row?.attributes.ph }}
@@ -47,6 +146,36 @@
             <q-td :props="props" class="">
               {{ displayDateOnly(props.row?.attributes.createdAt) }}
             </q-td>
+          </template>
+          <template v-slot:bottom>
+            <div
+              class="flex items-center justify-between"
+              style="width: 100% !important"
+            >
+              <div>
+                <span class="q-mr-md text-caption">
+                  {{ (currentPage - 1) * initialPagination.rowsPerPage + 1 }}
+                  -
+                  {{
+                    currentPage * initialPagination.rowsPerPage < totalRows
+                      ? currentPage * initialPagination.rowsPerPage
+                      : totalRows
+                  }}
+                  out of
+                  {{
+                    maxPage * initialPagination.rowsPerPage > totalRows
+                      ? totalRows
+                      : maxPage * initialPagination.rowsPerPage
+                  }}
+                </span>
+              </div>
+              <q-pagination
+                v-model="currentPage"
+                :max="maxPage"
+                input
+                @update:model-value="dateOnChange"
+              />
+            </div>
           </template>
         </q-table>
       </div>
@@ -81,6 +210,7 @@ import moment from "moment";
 import {
   displayTimeOnly,
   displayDateOnly,
+  displayDateOnly2,
 } from "src/composables/useDateFormatter";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
@@ -103,6 +233,16 @@ const phChartData = ref([]);
 const phChartDataLabel = ref([]);
 const last_reading_date = ref("");
 const rows = ref([]);
+const loading = ref(false);
+const currentPage = ref(1);
+const maxPage = ref(1);
+const totalRows = ref(0);
+const date_from = ref(
+  displayDateOnly2(new Date().setDate(new Date().getDate()))
+);
+const date_to = ref(displayDateOnly2(new Date().setDate(new Date().getDate())));
+const sort = ref("desc");
+const sort_options = ref(["asc", "desc"]);
 const columns = [
   {
     name: "ph",
@@ -129,7 +269,7 @@ const initialPagination = ref({
   // descending: false,
   page: 1,
   rowsPerPage: 10,
-  // rowsNumber: xx if getting data from a server
+  rowsNumber: 10,
 });
 
 let data_1 = [];
@@ -176,14 +316,13 @@ const updateCharts = () => {
 
 const initSocketIO = async () => {
   socket_IO.on(`ipond-dashboard`, async (data) => {
-    console.log(data);
+    // console.log(data);
     await getSensorData();
   });
 };
 
 const getSensorData = async () => {
   await fetchCurrentSensorData().then((res) => {
-    console.log(res);
     phLevel.value = res?.data.data[0]?.attributes.ph;
     // salinity.value = res?.data.data[0]?.attributes.sal;
     // temperature.value = res?.data.data[0]?.attributes.rtd;
@@ -192,9 +331,6 @@ const getSensorData = async () => {
   });
   await fetchSensorData()
     .then((res) => {
-      console.log(res);
-      console.log(res?.data);
-
       for (let i = 0; i <= res.data.data.length; i++) {
         phChartData.value[i] = res?.data.data[i]?.attributes.ph;
         phChartDataLabel.value[i] = moment(
@@ -212,22 +348,89 @@ const getSensorData = async () => {
     });
 };
 
-const getPH_Levels = async () => {
-  await getPHLevels()
+const getPH_Levels = async (dateFrom, dateTo, order) => {
+  loading.value = true;
+
+  const startRow =
+    (currentPage.value - 1) * initialPagination.value.rowsPerPage;
+
+  console.log("Page number", currentPage.value);
+  await getPHLevels(
+    dateFrom,
+    dateTo,
+    initialPagination.value.rowsPerPage,
+    startRow,
+    order
+  )
     .then((res) => {
-      console.log("PH", res);
-      rows.value = res?.data.data;
-      console.log("Rows", rows);
+      maxPage.value = Math.ceil(
+        res?.data.meta.pagination.total / res?.data.meta.pagination.limit
+      );
+      totalRows.value = res?.data.meta.pagination.total;
+
+      const rowsData = res?.data.data;
+      console.log("Rows Meta", res?.data.meta.pagination);
+      rows.value.splice(0, rows.value.length, ...rowsData);
     })
     .catch((err) => {
       console.log(err);
+    })
+    .finally(() => {
+      loading.value = false;
     });
 };
 
-onMounted(() => {
+const dateOnChange = () => {
+  const dateFrom = `${date_from.value.substring(
+    0,
+    4
+  )}-${date_from.value.substring(5, 7)}-${date_from.value.substring(8, 10)}`;
+  const dateTo = `${date_to.value.substring(0, 4)}-${date_to.value.substring(
+    5,
+    7
+  )}-${date_to.value.substring(8, 10)}`;
+
+  var DATE_FROM = new Date(dateFrom);
+  DATE_FROM.setDate(DATE_FROM.getDate());
+  var DATE_TO = new Date(dateTo);
+  DATE_TO.setDate(DATE_TO.getDate() + 1);
+
+  getPH_Levels(
+    displayDateOnly2(DATE_FROM),
+    displayDateOnly2(DATE_TO),
+    sort.value
+  );
+};
+
+const changeQueryParams = () => {
+  currentPage.value = 1;
+  dateOnChange();
+};
+onMounted(async () => {
   getSensorData();
-  getPH_Levels();
+  dateOnChange();
   initSocketIO();
 });
 </script>
-<style scoped></style>
+<style scoped>
+.date-from,
+.date-to {
+  max-width: 150px;
+}
+@media only screen and (max-width: 420px) {
+  .date-from,
+  .date-to {
+    max-width: 128px;
+  }
+}
+
+@media only screen and (max-width: 376px) {
+  .date-from,
+  .date-to {
+    max-width: 125px;
+  }
+  .sort-opt {
+    max-width: 70px;
+  }
+}
+</style>

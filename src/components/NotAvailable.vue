a91b0e9900c517898d1adede2848541399e4cddc
Roy <royrmagdaong@gmail.com>
2023-08-07 13:42:34 +0800

    pond 2-5 feature unavailasbl

---
+++ src/components/NotAvailable.vue
@@ -0,0 +1,10 @@
+a
+<template>
+  <q-layout>
+    <q-page-container class="flex flex-center fade-in" style="height: 90vh">
+      <div class="text-h2" style="color: tomato">
+        This feature is currently unavailable.
+      </div>
+    </q-page-container>
+  </q-layout>
+</template>

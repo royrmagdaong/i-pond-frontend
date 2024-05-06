21ffcd1d07f7d1971168ba286b4148bd8c901446
Roy <royrmagdaong@gmail.com>
2024-05-06 15:30:03 +0800

    change local server to cloud server

--- src/components/Pond2Humidity.vue
+++ src/components/Pond2Humidity.vue
@@ -218,8 +218,8 @@ ChartJS.register(
   Legend
 );
 
-// const socket_IO = socket("https://i-pond-backend.ap.ngrok.io", {});
-const socket_IO = socket("http://localhost:1338", {});
+const socket_IO = socket("https://i-pond-backend.ap.ngrok.io", {});
+// const socket_IO = socket("http://localhost:1338", {});
 // const socket_IO = socket(server_url, {});
 const phLevel = ref(0);
 const phChartData = ref([]);
